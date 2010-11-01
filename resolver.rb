0930645de661e542613fdbde6dc44925f3218bac
Michel Martens <michel@soveran.com>
2010-11-01 15:33:08 -0300

    Add Resolver class.

--- resolver.rb
+++ resolver.rb
@@ -16,18 +16,36 @@ def gemspec(name, version, dependencies = {})
   end
 end
 
-gemspec("a", 1, "x" => ">= 1")
-gemspec("b", 1, "x" => "1")
-gemspec("x", 1)
-gemspec("x", 2)
-
 test do
   assert_equal "1", gemspec("x", 1).version.to_s
   assert_equal ">= 1", gemspec("a", 1, "x" => ">= 1").runtime_dependencies.first.requirement.to_s
   assert_equal "= 1", gemspec("a", 1, "x" => "1").runtime_dependencies.first.requirement.to_s
 end
 
-__END__
+class Resolver
+  attr :a
+  attr :b
+
+  def initialize(gems)
+    @gems = gems
+    @a = Hash.new { |hash, key| hash[key] = [] }
+    @b = Hash.new { |hash, key| hash[key] = [] }
+
+    populate_lists
+  end
+
+private
+
+  def populate_lists
+    @gems.each do |gem|
+      @a[gem.name] << gem
+
+      gem.runtime_dependencies.each do |dep|
+        @b[dep.name] << gem
+      end
+    end
+  end
+end
 
 A = Hash.new { |hash, key| hash[key] = [] }
 B = Hash.new { |hash, key| hash[key] = [] }
@@ -54,31 +72,29 @@ B.each do |name, dependees|
   remove.concat(_remove).concat(keep[1..-1])
 end
 
-puts "Remove: #{remove.inspect}"
-
-test do
-  assert_equal "x", remove.first.name
-  assert_equal "2", remove.first.version.to_s
+setup do
+  [].tap do |gems|
+    gems << gemspec("a", 1, "x" => ">= 1")
+    gems << gemspec("b", 1, "x" => "1")
+    gems << gemspec("x", 1)
+    gems << gemspec("x", 2)
+  end
 end
 
-test do
-  assert_equal "1", A["a"].first.version.to_s
-  assert_equal "1", A["b"].first.version.to_s
-  assert_equal "1", A["x"].first.version.to_s
-  assert_equal "2", A["x"].last.version.to_s
+test do |gems|
+  resolver = Resolver.new(gems)
+  assert_equal "1", resolver.a["a"].first.version.to_s
+  assert_equal "1", resolver.a["b"].first.version.to_s
+  assert_equal "1", resolver.a["x"].first.version.to_s
+  assert_equal "2", resolver.a["x"].last.version.to_s
 end
 
-test do
-  assert_equal [],  B["a"]
-  assert_equal [],  B["b"]
-  assert_equal "a", B["x"].first.name
-  assert_equal "1", B["x"].first.version.to_s
-  assert_equal "b", B["x"].last.name
-  assert_equal "1", B["x"].last.version.to_s
+test do |gems|
+  resolver = Resolver.new(gems)
+  assert_equal [],  resolver.b["a"]
+  assert_equal [],  resolver.b["b"]
+  assert_equal "a", resolver.b["x"].first.name
+  assert_equal "1", resolver.b["x"].first.version.to_s
+  assert_equal "b", resolver.b["x"].last.name
+  assert_equal "1", resolver.b["x"].last.version.to_s
 end
-
-test do
-#  assert_equal({"a" => "1", "b" => "1", "x" => "1"}, resolved_dependencies)
-end
-
-puts
